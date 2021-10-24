67cbd2ce2672d22faab25e032ef09fefe01d310c
Sergey Nilov <nilov@softreactor.com>
2021-10-24 16:51:20 +0300

    Adding issue code

--- app/Http/Livewire/Hostel/HostelsHomepageSpotlight.php
+++ app/Http/Livewire/Hostel/HostelsHomepageSpotlight.php
@@ -6,22 +6,43 @@ use Livewire\Component;
 
 class HostelsHomepageSpotlight extends Component
 {
+    private $hostelsDataRows = [];
+
     public function render()
     {
-        \Log::info( '-1 HostelsHomepageSpotlight ::' . print_r( -1, true  ) );
+        \Log::info('-1 HostelsHomepageSpotlight ::' . print_r(-1, true));
 
 //        $this->emitUp('setCurrentPageProperties', [
         // $this->emit('setCurrentPageProperties', [
         //     'title'                 => 'Cal from HostelsHomepageSpotlight "',
         // ]);
 
-        return view('livewire.hostel.hostels-homepage-spotlight');
+        return view('livewire.hostel.hostels-homepage-spotlight', [
+            'hostelsDataRows' => $this->hostelsDataRows,
+
+        ]);
     }
 
+    public function mount()
+    {
+        \Log::info('-1 HostelsHomepageSpotlight MOUNT -99::' . print_r(-99, true));
+
+        $this->hostelsDataRows = [
+            ['id' => 1, 'label 1'],
+            ['id' => 2, 'label 2'],
+        ];
+    }
+
+
     public function emitHeaderToComponent()
-{
-    $this->emit('setCurrentPageProperties', [
-        'title' => 'Cal from ' . __CLASS__,
-    ]);
-}
+    {
+
+        $this->hostelsDataRows = [
+            ['id' => 5, 'label 5'],
+            ['id' => 6, 'label 6'],
+        ];
+        $this->emit('setCurrentPageProperties', [
+            'title' => 'Cal from ' . __CLASS__,
+        ]);
+    }
 }
--- resources/views/livewire/hostel/hostels-homepage-spotlight.blade.php
+++ resources/views/livewire/hostel/hostels-homepage-spotlight.blade.php
@@ -1,4 +1,5 @@
 <div wire:init="emitHeaderToComponent">
     resources/views/livewire/hostel/hostels-homepage-spotlight.blade.php<br>
     <a href="/hostel-view-page/slug"> Goto Hostel view</a>
+    $hostelsDataRows::{{ var_dump($hostelsDataRows) }}
 </div>
